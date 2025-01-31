1f8276369d426e8b9d0f64a90168a81cb84b49d0
Md Zubayer Hossain Patwori <101261770+mdzubayerhossain@users.noreply.github.com>
2025-02-01 05:28:59 +0600

    Update app.py

--- training/app.py
+++ training/app.py
@@ -1,51 +1,59 @@
-
-
+# Import necessary libraries
 import os
 import json
 import requests
 import time
 from flask import Flask, request, jsonify
+from inference import BanglaNER  # Import the BanglaNER class from the inference module
 
-
-from inference import BanglaNER
-
+# Set debug mode to True
 DEBUG = True
 
-
+# Initialize the Flask application
 app = Flask(__name__)
-class config:
-    model_path = "./models/bangla_ner_model"
 
-cfg = config()
+# Define a configuration class to hold the model path
+class Config:
+    model_path = "./models/bangla_ner_model"  # Path to the Bangla NER model
+
+# Create an instance of the configuration class
+cfg = Config()
+
+# Initialize the BanglaNER model with the model path
 bner = BanglaNER(cfg.model_path)
 
-
-
+# Define the NER endpoint
 @app.route('/ner', methods=['POST'])
 def bangla_ner():
     """
-    
+    Endpoint for Bangla NER.
     """
+    # Record the start time
     st = time.time()
+
+    # Get the JSON data from the request
     data = request.get_json()
-    sender = data.get('sender_id', '')
-    text = data.get('text', '')
+    sender = data.get('sender_id', '')  # Get the sender ID from the JSON data
+    text = data.get('text', '')  # Get the text from the JSON data
 
+    # Print the request details
     print("request : ", request)
-
     print(f"sender : {sender}")
     print(f"text : {text}")
 
+    # Get the prediction from the model
     prediction = bner.prediction(text)
+
+    # Prepare the response
     response = {
         "sender_id" : sender,
         "body"      : prediction,
         "status"    : 200
-        }
+    }
 
+    # Return the response as a JSON string
     return json.dumps(response, ensure_ascii=False, indent=2)
 
-
+# Run the Flask application
 if __name__ == '__main__':
-
-    app.run(debug=True, host="0.0.0.0",port=8008)
+    app.run(debug=True, host="0.0.0.0", port=8008)  # Run the app in debug mode on all available IP addresses and port 8008
